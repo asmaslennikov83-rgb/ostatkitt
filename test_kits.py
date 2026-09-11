4260a2b66e5f3794a69754cc2beb0fe82e97e7f6
asmaslennikov83-rgb <asmaslennikov83@gmail.com>
2026-09-11 14:32:27 +0300

    Add files via upload

---
+++ test_kits.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from app.kits import read_kits_xlsx
+
+kits = read_kits_xlsx(Path('templates/kits_template.xlsx'))
+assert len(kits) == 58
+sample = next(k for k in kits if k.barcode == '2054359985196')
+assert sample.components == ('4600987000343', '4600987000343')
+print('OK')
